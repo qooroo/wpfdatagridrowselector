9b150d58b7be6f207f0ea8a639d0cd2469e3de00
Qiming Liu <qooroo@gmail.com>
2015-07-20 16:52:53 +0100

    initial commit

---
+++ WpfDataGridFakeGrouping/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WpfDataGridFakeGrouping
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var vm = new Vm();
+            vm.Rows = new ObservableCollection<RowBase>
+            {
+                new GroupingRow {Header = "Grouped header"},
+                new Row {Name = "Bob", Number = 123},
+                new Row {Name = "Alice", Number = 456}
+            };
+
+            DataContext = vm;
+        }
+    }
+
+    public class Vm
+    {
+        public ObservableCollection<RowBase> Rows { get; set; }
+    }
+
+    public class RowBase
+    {
+    }
+
+    public class Row : RowBase
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+
+    public class GroupingRow : RowBase
+    {
+        public string Header { get; set; }
+    }
+}
